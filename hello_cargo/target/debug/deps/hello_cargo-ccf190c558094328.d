e82f596fe7e38aaa88aae13e3c8faa7532761197
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2025-03-23 15:59:24 -0400

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-ccf190c558094328.d
@@ -0,0 +1,5 @@
+/home/mnobre/Desktop/Study/rust-study/hello_cargo/target/debug/deps/hello_cargo-ccf190c558094328: src/main.rs
+
+/home/mnobre/Desktop/Study/rust-study/hello_cargo/target/debug/deps/hello_cargo-ccf190c558094328.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/mnobre/Desktop/Study/rust-study/hello_cargo/target/debug/hello_cargo: /home/mnobre/Desktop/Study/rust-study/hello_cargo/src/main.rs
